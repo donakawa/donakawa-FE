4a7d03f878c1a848f4013e80cb31bb493c0fcfec
yangyangeeee <sophia_ysy@naver.com>
2026-05-02 01:12:00 +0900

    🎉 Init: 프로젝트 초기 세팅

---
+++ app/(tabs)/_layout.tsx
@@ -0,0 +1,33 @@
+import { Ionicons } from "@expo/vector-icons";
+import { Tabs } from "expo-router";
+
+export default function TabLayout() {
+  return (
+    <Tabs
+      screenOptions={{
+        headerShown: false,
+        tabBarActiveTintColor: "#0B4112",
+      }}
+    >
+      <Tabs.Screen
+        name="index"
+        options={{
+          title: "Home",
+          tabBarIcon: ({ color, size }) => (
+            <Ionicons name="home" size={size} color={color} />
+          ),
+        }}
+      />
+
+      <Tabs.Screen
+        name="explore"
+        options={{
+          title: "Explore",
+          tabBarIcon: ({ color, size }) => (
+            <Ionicons name="paper-plane" size={size} color={color} />
+          ),
+        }}
+      />
+    </Tabs>
+  );
+}
